0cd1b55cd387fe764da2ec73f32c51d17da81dd4
Okoronkwo Charles <charlieonyii42@gmail.com>
2025-12-07 01:14:23 +0100

    Remove unused import for AggregatorV3Interface

--- src/SALVA.sol
+++ src/SALVA.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {SalvaBase} from "./SALVA_BASE_LIB.sol";
 
